d978babec0a4bab4770f908ec0144ed6d7ba794a
Ocillacubes <squirtle1607@rollingoak.com>
2021-09-07 13:36:25 -0500

    Start work on installer

---
+++ installer/main.c
@@ -0,0 +1,27 @@
+// Yes I'm using C to essentially spam bash commands. Deal with it.
+//
+/* Copyright (C) 2021 Ocillacubes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argC, char *argV[]) {
+    printf("Welcome to JunPogOS\n");
+    printf("To learn more about it, vists ocillacubes.com/JunPogOS\n");
+}
